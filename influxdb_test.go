3f1613ff555f7e623548dcd70ff192050541de82
Simon de Vlieger <cmdr@supakeen.com>
2024-05-18 16:18:35 +0200

    test: happy test
    Add a first happy test.
    
    Signed-off-by: Simon de Vlieger <cmdr@supakeen.com>

--- influxdb_test.go
+++ influxdb_test.go
@@ -1,1 +1,44 @@
 package quokka_test
+
+import (
+	"testing"
+    "reflect"
+
+	"github.com/petspalace/quokka"
+)
+
+func TestNewInflux(t *testing.T) {
+	type test struct {
+		input string
+		want *quokka.InfluxMessage
+        err error
+	}
+
+	tests := []test{
+		{
+			input: "temperature,tag=tag-value field=field-value",
+			want: &quokka.InfluxMessage{
+				Name: "temperature",
+				Tags: map[string]string{
+					"tag": "tag-value",
+				},
+				Fields: map[string]string{
+					"field": "field-value",
+				},
+			},
+            err: nil,
+		},
+	}
+
+    for _, tc := range tests {
+        res, err := quokka.NewInflux(tc.input)
+
+        if err != tc.err {
+            t.Fatalf("expected err: %v got: %v", tc.want, err)
+        }
+
+        if !reflect.DeepEqual(tc.want, res) {
+            t.Fatalf("expected: %v got: %v", tc.want, res)
+        }
+    }
+}
